48af903872157a193d376c89eee22448809ef134
karimhozaien <karimhozaien@gmail.com>
2024-09-11 21:28:06 -0400

    generating a key

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,15 @@
-from cyryptography.fernet import fernet
+from cryptography.fernet import Fernet
+
+class PasswordManager:
+
+    def __init__(self):
+        self.key = None
+        self.password_file = None
+        self.password_dict = {}
+
+    def create_key(self, path):
+        self.key = Fernet.generate_key()
+        print(self.key)
+
+pm = PasswordManager()
+pm.create_key(None)
